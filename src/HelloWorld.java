23a58f0229fc19b4e00823d8bdca583a73dc7cd4
BT_3W2_16 <andrej42@nycstudents.net>
2023-09-26 08:38:12 -0400

    Changed print statement

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class HelloWorld {
+public class HelloWorld {
+public static void main(String[] args) {
+    String a = "September";
+    String b = "2023";
+    String c = "26";
+    System.out.println("Persona 5 Royal is one of the best games of all time.");
+    System.out.println("Today is " + a + " " + c +"th " + b);
+
+    }
 }
